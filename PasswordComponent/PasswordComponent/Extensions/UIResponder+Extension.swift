2c21d37ababd4d1944e228ebe73fbc6f6df40efd
Gizem Boskan <gizem.boskan.dev@gmail.com>
2022-04-07 10:37:58 +0300

    Complete the setup for keyboard hiding

---
+++ PasswordComponent/PasswordComponent/Extensions/UIResponder+Extension.swift
@@ -0,0 +1,28 @@
+//
+//  UIResponder+Extension.swift
+//  PasswordComponent
+//
+//  Created by Gizem Boskan on 6.04.2022.
+//
+
+import UIKit
+
+extension UIResponder {
+    private struct Static {
+        static weak var responder: UIResponder?
+    }
+    
+    /**
+     Finds the current first responder
+     - Returns: the current UIResponder if it exists
+     */
+    static func currentFirst() -> UIResponder? {
+        Static.responder = nil
+        UIApplication.shared.sendAction(#selector(UIResponder._trap), to: nil, from: nil, for: nil)
+        return Static.responder 
+    }
+    
+    @objc private func _trap() {
+        Static.responder = self
+    }
+}
